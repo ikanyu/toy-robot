5c310fc40c5c06b2b5e2db62e034dabb896cb585
Wen Yu <wongwy93@gmail.com>
2016-05-06 04:22:56 +0800

    Convert tabs to spaces

--- robot.rb
+++ robot.rb
@@ -4,72 +4,72 @@ require_relative 'toy_helper.rb'
 
 class Robot
 
-	include ActiveModel::Model
-	include ToyHelper
+  include ActiveModel::Model
+  include ToyHelper
 
-	attr_accessor :x_coordinate, :y_coordinate, :current_direction, :commands, :report
+  attr_accessor :x_coordinate, :y_coordinate, :current_direction, :commands, :report
 
-	def initialize(filename)
-		@direction = ['NORTH', 'EAST', 'SOUTH', 'WEST']
-		@commands = filename
-		@x_coordinate = 0
-		@y_coordinate = 0
-	end
+  def initialize(filename)
+    @direction = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+    @commands = filename
+    @x_coordinate = 0
+    @y_coordinate = 0
+  end
 
-	def start_game
-		@commands.each do |command|
-			data = command.split(' ')
-			action = data[0]
-			if action.upcase == 'PLACE'
-				@x_coordinate, @y_coordinate, @current_direction = data[1].split(',')
-				@x_coordinate = @x_coordinate.to_i
-				@y_coordinate = @y_coordinate.to_i
-				@current_direction = @current_direction.upcase
-				if @x_coordinate < 0 || @y_coordinate < 0
-					@x_coordinate = 0
-					@y_coordinate = 0
-					@current_direction = nil
-					puts 'X and Y coordinate must be bigger than 0!'
-				end
-			elsif action.upcase == 'LEFT'
-				left
-			elsif action.upcase == 'RIGHT'
-				right
-			elsif action.upcase == 'REPORT'
-				report
-			elsif action.upcase == 'MOVE'
-				move
-			else
-				return
-			end
-		end
-		@report
-	end
+  def start_game
+    @commands.each do |command|
+      data = command.split(' ')
+      action = data[0]
+      if action.upcase == 'PLACE'
+        @x_coordinate, @y_coordinate, @current_direction = data[1].split(',')
+        @x_coordinate = @x_coordinate.to_i
+        @y_coordinate = @y_coordinate.to_i
+        @current_direction = @current_direction.upcase
+        if @x_coordinate < 0 || @y_coordinate < 0
+          @x_coordinate = 0
+          @y_coordinate = 0
+          @current_direction = nil
+          puts 'X and Y coordinate must be bigger than 0!'
+        end
+      elsif action.upcase == 'LEFT'
+        left
+      elsif action.upcase == 'RIGHT'
+        right
+      elsif action.upcase == 'REPORT'
+        report
+      elsif action.upcase == 'MOVE'
+        move
+      else
+        return
+      end
+    end
+    @report
+  end
 
-	def left
-		return unless placed_on_board?
-		position = @direction.find_index(@current_direction)
-		@current_direction = @direction[position - 1]
-	end
+  def left
+    return unless placed_on_board?
+    position = @direction.find_index(@current_direction)
+    @current_direction = @direction[position - 1]
+  end
 
-	def right
-		return unless placed_on_board?
-		position = @direction.find_index(@current_direction)
-		position == 3 ? @current_direction = @direction[0] : @current_direction = @direction[position + 1]
-	end
+  def right
+    return unless placed_on_board?
+    position = @direction.find_index(@current_direction)
+    position == 3 ? @current_direction = @direction[0] : @current_direction = @direction[position + 1]
+  end
 
-	def move
-		return unless placed_on_board?
-		check_danger_and_move(@current_direction)
-	end
+  def move
+    return unless placed_on_board?
+    check_danger_and_move(@current_direction)
+  end
 
-	def report
-		if placed_on_board?
-			@report = "#{@x_coordinate},#{@y_coordinate},#{@current_direction}" 
-			puts @report
-		else
-			puts "Toy not placed on board"
-		end
-	end
+  def report
+    if placed_on_board?
+      @report = "#{@x_coordinate},#{@y_coordinate},#{@current_direction}" 
+      puts @report
+    else
+      puts "Toy not placed on board"
+    end
+  end
 
 end
--- spec/toyrobot_spec.rb
+++ spec/toyrobot_spec.rb
@@ -1,35 +1,35 @@
 require_relative '../robot.rb'
 
-describe Robot do	
+describe Robot do 
 
-	describe 'Start Game' do
-		
-		context 'Robot on the table' do
-			let!(:file) { File.readlines('test1.txt') }
-			it 'displays current position' do
-				robot = Robot.new(file)
-				robot.start_game
-				expect(robot.start_game).to eq('0,1,NORTH')
-			end
-		end
+  describe 'Start Game' do
+    
+    context 'Robot on the table' do
+      let!(:file) { File.readlines('test1.txt') }
+      it 'displays current position' do
+        robot = Robot.new(file)
+        robot.start_game
+        expect(robot.start_game).to eq('0,1,NORTH')
+      end
+    end
 
-		context 'Robot not on table' do
-			let!(:file) { File.readlines('test7.txt') }
-			it 'shows reminder' do
-				robot = Robot.new(file)
-				robot.start_game
-				expect(robot.errors.full_messages).to include('Please place robot on the board first!')
-			end
-		end
+    context 'Robot not on table' do
+      let!(:file) { File.readlines('test7.txt') }
+      it 'shows reminder' do
+        robot = Robot.new(file)
+        robot.start_game
+        expect(robot.errors.full_messages).to include('Please place robot on the board first!')
+      end
+    end
 
-		context 'Stop robot from falling' do
-			let!(:file) { File.readlines('test8.txt') }
-			it 'remains at same place' do
-				robot = Robot.new(file)
-				robot.start_game
-				expect(robot.start_game).to eq('0,0,WEST')
-			end
-		end
+    context 'Stop robot from falling' do
+      let!(:file) { File.readlines('test8.txt') }
+      it 'remains at same place' do
+        robot = Robot.new(file)
+        robot.start_game
+        expect(robot.start_game).to eq('0,0,WEST')
+      end
+    end
 
-	end
+  end
 end
